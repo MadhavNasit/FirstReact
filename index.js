ef0fd2d418f44689407d47f971fe4079b890e71d
MadhavNasit <40732694+MadhavNasit@users.noreply.github.com>
2020-09-03 01:36:58 +0530

    Flex-Screen Initial Design

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ import App from './App';
 import { name as appName } from './app.json';
 import HomeScreen from './App/home-screen/home-screen';
 import ContactMe from './App/contact-me/contact-me';
+import FlexScreen from './App/flex-practice/flex-screen';
 
-
-AppRegistry.registerComponent(appName, () => HomeScreen);
+AppRegistry.registerComponent(appName, () => FlexScreen);
